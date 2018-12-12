865c5ee98205e8de3faa04e586589e3e48077044
Jure Baban <baban.jure@gmail.com>
2018-12-12 17:27:31 +0100

    updated twitter baseline to properly utilize train and dev sets

--- baseline.py
+++ baseline.py
@@ -73,5 +73,69 @@ def baseline_twitter():
     print('ratio', counter / counter_all)
 
 
+def baseline_twitter_train_dev():
+    data = pd.read_pickle('twitter.pkl')
+
+    x_text = []
+    y = []
+    counter = 0
+    counter_all = 0
+    for base_text in data['train']:
+
+        x_text.append(base_text['source']['text'])
+        y.append(base_text['source']['label'])
+
+        for reply in base_text['replies']:
+            # print(reply)
+            counter_all += 1
+            if pd.notnull(reply['text']) and reply['text'] != '[deleted]' and reply['text'] != '[removed]':
+                x_text.append(reply['text'])
+                y.append(reply['label'])
+                if reply['label'] == 'comment':
+                    counter += 1
+
+    tfidf = TfidfVectorizer()
+    x = tfidf.fit_transform(x_text)
+
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.model_selection import train_test_split
+    from sklearn.metrics import accuracy_score
+
+    x_text = []
+    y_dev = []
+    counter = 0
+    counter_all = 0
+    for base_text in data['dev']:
+
+        x_text.append(base_text['source']['text'])
+        y_dev.append(base_text['source']['label'])
+
+        counter_all += 1
+        if base_text['source']['label'] == 'comment':
+            counter += 1
+
+        for reply in base_text['replies']:
+            # print(reply)
+            counter_all += 1
+            if pd.notnull(reply['text']) and reply['text'] != '[deleted]' and reply['text'] != '[removed]':
+                x_text.append(reply['text'])
+                y_dev.append(reply['label'])
+                if reply['label'] == 'comment':
+                    counter += 1
+
+    x_dev = tfidf.transform(x_text)
+
+    rf = RandomForestClassifier(n_estimators=20)
+
+    rf.fit(x, y)
+    pred = rf.predict(x_dev)
+    print('acc', accuracy_score(y_dev, pred))
+    print(pred)
+
+    print('comments counter', counter)
+    print('all labels counter', counter_all)
+    print('ratio', counter / counter_all)
+
+
 if __name__ == "__main__":
-    baseline_twitter()
+    baseline_twitter_train_dev()
